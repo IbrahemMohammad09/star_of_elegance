c3816abb8dd9d74740b80dd2506af00392d09add
adeeb <adeebmoualla748@gmail.com>
2025-01-29 12:57:58 +0300

    Install Tallwind V3

--- Star-Of-Elegance/src/App.jsx
+++ Star-Of-Elegance/src/App.jsx
@@ -1,12 +1,13 @@
-import './App.css'
+import "./App.css";
 
 function App() {
-
-
   return (
     <>
+      <div>
+        <h1 className="text-black">This is The First Page</h1>
+      </div>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
