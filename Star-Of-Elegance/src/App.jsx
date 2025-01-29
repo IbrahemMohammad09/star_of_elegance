a610527db2f4c37a8f061415b14a4d1f55f25978
IbrahemMohammad09 <137792138+IbrahemMohammad09@users.noreply.github.com>
2025-01-30 01:14:33 +0300

    add footer

--- Star-Of-Elegance/src/App.jsx
+++ Star-Of-Elegance/src/App.jsx
@@ -1,4 +1,5 @@
 import "./App.css";
+import Footer from "./components/Footer";
 import HeroSection from "./sections/HeroSection";
 
 function App() {
@@ -6,6 +7,7 @@ function App() {
     <>
       <div>
         <HeroSection />
+        <Footer />
       </div>
     </>
   );
