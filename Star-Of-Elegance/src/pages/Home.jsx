79a33ae496b1ba753932b43bc01e916d775b7ab0
IbrahemMohammad09 <137792138+IbrahemMohammad09@users.noreply.github.com>
2025-01-31 01:06:50 +0300

    add about us

--- Star-Of-Elegance/src/pages/Home.jsx
+++ Star-Of-Elegance/src/pages/Home.jsx
@@ -1,11 +1,15 @@
 import React from 'react'
 import HeroSection from '../sections/HeroSection'
 import Footer from '../components/Footer'
+import ServicesSection from '../sections/ServicesSection'
+import ProjectsSection from '../sections/ProjectsSection'
 
 const Home = () => {
   return (
     <div>
         <HeroSection />
+        <ServicesSection />
+        <ProjectsSection />
         <Footer />
     </div>
   )
