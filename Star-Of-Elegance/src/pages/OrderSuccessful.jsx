93fdf54bba5c46492059431540dd440a899ac507
IbrahemMohammad09 <137792138+IbrahemMohammad09@users.noreply.github.com>
2025-01-31 23:44:08 +0300

    add about us

---
+++ Star-Of-Elegance/src/pages/OrderSuccessful.jsx
@@ -0,0 +1,21 @@
+import Successful from '../components/sharedComponents/Successful';
+import Navbar from '../components/Navbar';
+import Footer from '../components/Footer';
+import Title from '../components/sharedComponents/Title';
+
+export default function OrderSuccessful(){
+    return<>
+    <Navbar/>
+    <Title/>
+    <div>
+            <Successful 
+                title="Order Successful" 
+                message="Your request has been sent successfully." 
+                message2="Thank you for choosing us! " 
+                
+            />
+        </div>
+    <Footer/>
+
+    </>
+}
